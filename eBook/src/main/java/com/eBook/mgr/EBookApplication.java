372d1a57b4af571a5c2e7d5a51ca5228ae440c3f
aloha <floaholic@nate.com>
2020-05-17 17:17:41 +0900

    aa

---
+++ eBook/src/main/java/com/eBook/mgr/EBookApplication.java
@@ -0,0 +1,13 @@
+package com.eBook.mgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookApplication.class, args);
+	}
+
+}
