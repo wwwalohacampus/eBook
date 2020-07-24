eebd752ffaf209da1d6f9781cc2a39cfa1884ff3
admin <admin@DESKTOP-C0PDQN1>
2020-07-24 15:14:00 +0900

    로그인 비활성화 로그인안되도록 처리

--- eBook/src/main/java/com/eBook/mgr/common/security/CustomUserDetailsService.java
+++ eBook/src/main/java/com/eBook/mgr/common/security/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package com.eBook.mgr.common.security;
 import com.eBook.mgr.common.security.domain.CustomUser;
 import com.eBook.mgr.domain.Member;
 import com.eBook.mgr.mapper.MemberMapper;
+import com.eBook.mgr.service.MemberService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +18,32 @@ public class CustomUserDetailsService implements UserDetailsService {
 
 	@Autowired
 	private MemberMapper memberMapper;
+	
+	@Autowired
+	private MemberService memberService;
 
 	@Override
 	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 		
 
 		logger.warn("Load User By UserName : " + userName);
+		try {
+			String state = memberService.readStatus(userName);
+			System.out.println("로그인 스테이트 : " + state);
+			
+			if(state.equals("1")) {
+				Member member = memberMapper.readByUserId(userName);
+				
+				logger.warn("queried by member mapper: " + member);
 
-		Member member = memberMapper.readByUserId(userName);
-
-		logger.warn("queried by member mapper: " + member);
-
-		return member == null ? null : new CustomUser(member);
+				return member == null ? null : new CustomUser(member);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	} 
 
 }
