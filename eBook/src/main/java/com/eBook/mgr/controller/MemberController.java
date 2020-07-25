99f2b5ed31b9133cf4b08ffd19e17099cb72e881
admin <admin@DESKTOP-C0PDQN1>
2020-07-25 20:24:33 +0900

    버튼 유저는 안보이도록 설정 회원수정 회원은 수정안되도록 설정 회원 등록, 회원수정 레이아웃 추가

--- eBook/src/main/java/com/eBook/mgr/controller/MemberController.java
+++ eBook/src/main/java/com/eBook/mgr/controller/MemberController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
